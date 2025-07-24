44c1879e8a411566db06cdf29a17a9f335ed07e8
SamairaGarg <gargsamaira4@gmail.com>
2025-07-24 09:54:47 +0530

    Re-added LECTURE 6, 7, and Lecture new as normal folders

---
+++ lecture6/index.js
@@ -0,0 +1,100 @@
+<<<<<<< HEAD
+// function starter(cb)
+// {
+//   console.log("starter order");
+//   setTimeout(()=>
+//   {
+//     console.log("starter served")
+//     cb(sweets)
+//     },1000)
+
+// }
+// function maincourse(cb){
+//      console.log("maincourse order");
+//   setTimeout(()=>
+//   {
+//     console.log("maincourse served")
+//     cb(bill)
+//   },1000)
+
+// }
+// function sweets()
+// {
+//      console.log("sweet order")
+//   setTimeout(()=>
+//   {
+//     console.log("sweet served")
+   
+//   },1000)
+
+// }
+// function bill()
+// {
+//      console.log("your bill is paid");
+//   setTimeout(()=>
+//   {
+//     console.log("bill paid");
+//   },1000)
+
+// }
+// starter(maincourse) //callback ki help se starter ke baad is func ko call krvaya
+// maincourse()
+// sweets()
+// bill();
+
+
+//*************
+function starter(cb) {
+  console.log("starter order");
+  setTimeout(() => {
+    console.log("starter served");
+    cb(); // Call maincourse
+  }, 1000);
+}
+
+function maincourse(cb) {
+  console.log("maincourse order");
+  setTimeout(() => {
+    console.log("maincourse served");
+    cb(); // Call sweets
+  }, 1000);
+}
+
+function sweets(cb) {
+  console.log("sweet order");
+  setTimeout(() => {
+    console.log("sweet served");
+    cb(); // Call bill
+  }, 1000);
+}
+
+function bill(cb) {
+  console.log("your bill is paid");
+  setTimeout(() => {
+    console.log("bill paid");
+    cb(); // Final hello
+  }, 1000);
+}
+
+starter(function () {
+  maincourse(function () {
+    sweets(function () {
+      bill(function () {
+        console.log("hello");
+      });
+    });
+  });
+});
+=======
+const pack=require ("samg14");
+console.log(pack);
+
+let sum = pack.sum(2,4)
+console.log(sum);
+
+let multiple = pack.multiple(2,4)
+console.log(multiple);
+
+let divide = pack.divide(2,4)
+console.log(divide);
+>>>>>>> 6cb3acb033c705f8867125d5ba896a181b7da846
